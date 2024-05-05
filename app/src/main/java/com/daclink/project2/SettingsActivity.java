0a0e82bdc6b3b4407a964c30e0dead8fbf92981a
JaysonB4 <jbasilio@csumb.edu>
2024-05-05 00:42:14 -0700

    In the middle of doing settings activity

---
+++ app/src/main/java/com/daclink/project2/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.daclink.project2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.daclink.project2.databinding.ActivitySettingsBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_settings);
+    }
+
+    static Intent settingsActivityIntentFactory(Context context) {
+        return new Intent(context, SettingsActivity.class);
+    }
+}
